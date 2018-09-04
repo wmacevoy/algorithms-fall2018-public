7bfc14a6328bfd7d8a21ee9a3d8c73d462882905
Warren MacEvoy <wmacevoy@gmail.com>
2018-09-04 16:54:57 -0600

    sample estimate

--- algorithms/src/main/java/csci480/Stats.java
+++ algorithms/src/main/java/csci480/Stats.java
@@ -5,38 +5,71 @@
  */
 package csci480;
 
+import static org.apache.commons.math3.special.Erf.erfcInv;
+
 /**
  *
  * @author wmacevoy
  */
 public class Stats {
+
     public int samples;
-    public double sum,sum2,min,max;
-    public Stats() { reset(); }
-    public void reset() { samples = 0; sum=0; sum2=0; min = Double.NaN; max = Double.NaN; }
+    public double sum, sum2, min, max;
 
-  public void sample(double value) {
-    if (samples == 0) {
-      samples = 1;
-      sum = value;
-      sum2 = value*value;
-      min = value;
-      max = value;
-    } else {
-      ++samples;
-      sum += value;
-      sum2 += value*value;
-      if (value < min) min = value;
-      if (value > max) max = value;
+    public Stats() {
+        reset();
     }
-  }
 
-  public double mean() {
-    return sum/samples;
-  }
+    public void reset() {
+        samples = 0;
+        sum = 0;
+        sum2 = 0;
+        min = Double.NaN;
+        max = Double.NaN;
+    }
 
-  public double stddev() {
-    return Math.sqrt((sum2-Math.pow(sum,2)/samples)/(samples-1.0));
-  }
+    public void sample(double value) {
+        if (samples == 0) {
+            samples = 1;
+            sum = value;
+            sum2 = value * value;
+            min = value;
+            max = value;
+        } else {
+            ++samples;
+            sum += value;
+            sum2 += value * value;
+            if (value < min) {
+                min = value;
+            }
+            if (value > max) {
+                max = value;
+            }
+        }
+    }
+
+    public double mean() {
+        return sum / samples;
+    }
+
+    public double stddev() {
+        return Math.sqrt((sum2 - Math.pow(sum, 2) / samples) / (samples - 1.0));
+    }
+
+    static final double MIN_SAMPLES = 30.0;
+
+    public static double requiredSamples(double relativeError, double xbar, double sigma) {
+        double zMax = erfcInv(relativeError);
+        double nMax = Math.pow(zMax * sigma / (xbar * relativeError), 2.0);
+        return Math.max(nMax, MIN_SAMPLES);
+    }
+
+    public double requiredSamples(double relativeError) {
+        return requiredSamples(relativeError, mean(), stddev());
+    }
+
+    public double requiredSamples() {
+        return requiredSamples(0.05);
+    }
 
 }
