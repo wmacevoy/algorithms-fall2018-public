b6f28cc8befb80bf8f946f9deb86fc983e672eed
Warren MacEvoy <wmacevoy@gmail.com>
2018-09-12 12:04:10 -0600

    impl reduction

--- natjecanje/src/main/java/com/github/wmacevoy/natjecanje/Natjecanje.java
+++ natjecanje/src/main/java/com/github/wmacevoy/natjecanje/Natjecanje.java
@@ -6,24 +6,23 @@
 package com.github.wmacevoy.natjecanje;
 
 // https://open.kattis.com/problems/natjecanje
-
 import java.util.Scanner;
-
 
 /**
  *
  * @author wmacevoy
  */
 public class Natjecanje {
-    
-    public static void main(String [] args) {
+
+    public static void main(String[] args) {
         Natjecanje app = new Natjecanje();
         app.run();
     }
     int teams;
     int damaged;
     int reserves;
-    boolean [] haveBoat,haveReserve;
+    boolean[] haveBoat, haveReserve;
+
     void read() {
         Scanner in = new Scanner(System.in);
         teams = in.nextInt();
@@ -31,20 +30,66 @@ public class Natjecanje {
         reserves = in.nextInt();
         haveBoat = new boolean[teams];
         haveReserve = new boolean[teams];
-        for (int i=0; i<teams; ++i) { haveBoat[i]=true; }
-        for (int i=0; i<damaged; ++i) {
-            int team = in.nextInt();
-            haveBoat[team-1]=false;
+        for (int i = 0; i < teams; ++i) {
+            haveBoat[i] = true;
         }
-        for (int i=0; i<damaged; ++i) {
+        for (int i = 0; i < damaged; ++i) {
             int team = in.nextInt();
-            haveReserve[team-1]=true;
+            haveBoat[team - 1] = false;
+        }
+        for (int i = 0; i < damaged; ++i) {
+            int team = in.nextInt();
+            haveReserve[team - 1] = true;
         }
     }
-    
+
+    void reduce1() { // use spares where team needs thiers
+        for (int i = 0; i < teams; ++i) {
+            if (!haveBoat[i] && haveReserve[i]) {
+                haveBoat[i] = true;
+                haveReserve[i] = false;
+            }
+        }
+    }
+
+    void reduce2() { // eliminate spares that don't matter or must go 1 place
+        for (int i = 0; i < teams; ++i) {
+            if (haveReserve[i]) {
+                int uses = 0;
+                if (i > 0 && !haveBoat[i - 1]) {
+                    ++uses;
+                }
+                if (i + 1 < teams && !haveBoat[i + 1]) {
+                    ++uses;
+                }
+                if (uses == 0) {
+                    haveReserve[i] = false;
+                    --reserves;
+                } else if (uses == 1) {
+                    if (i > 0 && !haveBoat[i - 1]) {
+                        haveBoat[i - 1] = true;
+                        haveReserve[i] = false;
+                        --reserves;
+                        i = Math.min(-1, i - 3);
+                    }
+                    if (i + 1 < teams && !haveBoat[i + 1]) {
+                        haveBoat[i + 1] = true;
+                        haveReserve[i] = false;
+                        --reserves;
+                    }
+                }
+            }
+        }
+    }
+
+    void solve() {
+        reduce1();
+        reduce2();
+    }
+
     void run() {
         read();
         solve();
-        write();
+//        write();
     }
 }
